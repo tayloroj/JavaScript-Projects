1455d13e69dc16d40e6e453a6743f5ab9f2c6bd5
tayloroj <andrew.taylor_22@yahoo.com>
2023-02-28 16:56:00 +0000

    JS Assignment Submission # 1
    •  Use the window.alert() and document.write() methods.
    •  Assign string values to two variables and concatenate the variables together.
    •  Assign a color to at least one variable using the fontcolor() method.
    •  Write an expression.
    •  Add comments throughout your JavaScript explaining your code.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,29 @@
+window.alert("Hello, World!"); //----Assignment #4, creates a popup alerting window ----//
+
+document.write('Hello, World again! '); //----Assignment #5, displays text on the webpage through js ----//
+
+var A = "This is a string. "; //----Assignment #6, defining a variable and giving it a string value ----//
+document.write(A); //----Display the variable on the webpage----//
+
+var B = "This is also a string!"; //----Assignment #7, defining a variable and giving it a string value----//
+window.alert(B); //----Display the variable in an alert window----//
+
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded. "); //----Assignment #8, Using escape character to display apostrophe and/or quotation marks----//
+
+document.write("\"Be who you are and say what you feel," //----Assignment #9, Concatenating strings ----//
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ " -Dr. Seuss ");
+
+var C = "Concatenated" + " String " //----Challenge #1, defining a variable with concatenation----//
+document.write(C); //----Display the variable on the webpage----//
+
+var Family = "The Arezzinis", Dad = "Jeremiah ", Mom = "Hermoine ", Daughter = "Penny", Son = "Zorro"; //----Assignment #10, assigning multiple variables in one statement----//
+document.write(Dad); //----Display the variable on the webpage----//
+
+var Dad = Dad.fontcolor("red") //----Assignment #11, Using the fontcolor method on the variable----//
+var Mom = Mom.fontcolor("green")
+var Daughter = Daughter.fontcolor("blue")
+var Son = Son.fontcolor("yellow")
+document.write(Mom)
+
+document.write(3+3); //----Assignment #12, Example of an expression----//
